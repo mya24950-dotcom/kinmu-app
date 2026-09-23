72c1263f991ae5a95218af6a5e1c67aeea5a6970
mya24950-dotcom <mya24950@gmail.com>
2026-09-23 18:44:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3295,11 +3295,18 @@ deleteShiftButton.style.minWidth = "100%";
 deleteShiftButton.style.gridColumn = "1 / -1";
 deleteShiftButton.style.boxSizing = "border-box";
 
+// キャンセルボタンと同じサイズ
+deleteShiftButton.style.height = cancelButton.offsetHeight + "px";
+deleteShiftButton.style.padding = cancelButton.style.padding;
+deleteShiftButton.style.fontSize = cancelButton.style.fontSize;
+deleteShiftButton.style.lineHeight = cancelButton.style.lineHeight;
+deleteShiftButton.style.fontWeight = cancelButton.style.fontWeight;
+
+// 色だけ勤務削除用に変更
 deleteShiftButton.style.background = "#6f42c1";
 deleteShiftButton.style.color = "#ffffff";
 deleteShiftButton.style.borderColor = "#59339d";
-deleteShiftButton.style.fontWeight = "700";
-deleteShiftButton.style.fontSize = "14px";
+
 deleteShiftButton.addEventListener("click", async () => {
 
   // 勤務形態だけ削除
