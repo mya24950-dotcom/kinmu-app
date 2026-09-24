3ff3bdc8895237c1b27abaf254b0e3f97f311e1a
mya24950-dotcom <mya24950@gmail.com>
2026-09-24 19:05:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kinmu-app-v3";
+const CACHE_NAME = "kinmu-app-v5";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,51 +8,124 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
 
-  self.skipWaiting();
-});
+/* =========================================================
+   インストール
+========================================================= */
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener(
+  "install",
+  event => {
 
-  self.clients.claim();
-});
+    event.waitUntil(
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
+      caches
+        .open(CACHE_NAME)
+        .then(
+          cache => {
 
-        const responseClone =
-          response.clone();
+            return cache.addAll(
+              FILES_TO_CACHE
+            );
 
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(
-            event.request,
-            responseClone
-          );
-        });
+          }
+        )
 
-        return response;
-      })
-      .catch(() => {
-        return caches.match(
-          event.request
-        );
-      })
-  );
-});
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+/* =========================================================
+   有効化
+========================================================= */
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(
+          keys => {
+
+            return Promise.all(
+
+              keys
+                .filter(
+                  key =>
+                    key !== CACHE_NAME
+                )
+                .map(
+                  key =>
+                    caches.delete(key)
+                )
+
+            );
+
+          }
+        )
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+/* =========================================================
+   ファイル取得
+========================================================= */
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(
+          response => {
+
+            const responseClone =
+              response.clone();
+
+            caches
+              .open(CACHE_NAME)
+              .then(
+                cache => {
+
+                  cache.put(
+                    event.request,
+                    responseClone
+                  );
+
+                }
+              );
+
+            return response;
+
+          }
+        )
+
+        .catch(
+          () => {
+
+            return caches.match(
+              event.request
+            );
+
+          }
+        )
+
+    );
+
+  }
+);
