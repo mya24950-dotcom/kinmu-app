4f91a0b4e85969d5587f4e3688671daba4f015a0
mya24950-dotcom <mya24950@gmail.com>
2026-09-26 15:23:49 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,131 +1,15 @@
-const CACHE_NAME = "kinmu-app-v5";
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
-];
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => caches.delete(key))
+      )
+    )
+  );
 
-
-/* =========================================================
-   インストール
-========================================================= */
-
-self.addEventListener(
-  "install",
-  event => {
-
-    event.waitUntil(
-
-      caches
-        .open(CACHE_NAME)
-        .then(
-          cache => {
-
-            return cache.addAll(
-              FILES_TO_CACHE
-            );
-
-          }
-        )
-
-    );
-
-    self.skipWaiting();
-
-  }
-);
-
-
-/* =========================================================
-   有効化
-========================================================= */
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches
-        .keys()
-        .then(
-          keys => {
-
-            return Promise.all(
-
-              keys
-                .filter(
-                  key =>
-                    key !== CACHE_NAME
-                )
-                .map(
-                  key =>
-                    caches.delete(key)
-                )
-
-            );
-
-          }
-        )
-
-    );
-
-    self.clients.claim();
-
-  }
-);
-
-
-/* =========================================================
-   ファイル取得
-========================================================= */
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      fetch(event.request)
-
-        .then(
-          response => {
-
-            const responseClone =
-              response.clone();
-
-            caches
-              .open(CACHE_NAME)
-              .then(
-                cache => {
-
-                  cache.put(
-                    event.request,
-                    responseClone
-                  );
-
-                }
-              );
-
-            return response;
-
-          }
-        )
-
-        .catch(
-          () => {
-
-            return caches.match(
-              event.request
-            );
-
-          }
-        )
-
-    );
-
-  }
-);
+  self.clients.claim();
+});
